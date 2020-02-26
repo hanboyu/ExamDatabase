8cdfa8126454b923140c3886bd4fa1571b94dbca
Bill Han <hanb4@rpi.edu>
2020-02-26 17:53:46 -0500

    Displaying search result function is in progress.

--- src/main/resources/static/js/SearchResultsUtility.js
+++ src/main/resources/static/js/SearchResultsUtility.js
@@ -1,5 +1,36 @@
-function MagnifyQuestion(){
+var question_container = document.getElementById("magnified_question_container");
+var close_button = close_button = document.getElementById("magnified_question_close_button");
 
+// When the user clicks on <span> (x), close the modal
+close_button.onclick = function() {
+    question_container.style.display = "none";};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === question_container) {
+        question_container.style.display = "none";
+    }
+};
+
+function MagnifyQuestion(id){
+    question_container.style.display = "block";
+}
+
+function ExpendQuestion(question_id) {
+    let q = document.getElementById(question_id);
+    let choices = q.getElementsByClassName("rq_choices_container")[0];
+    choices.style.display = "block";
+    let arrow = q.getElementsByClassName("arrow");
+    arrow[0].style.display = "none";
+    arrow[1].style.display = "inline-block";
+}
+
+function CollapseQuestion(question_id){
+    let q = document.getElementById(question_id);
+    let choices = q.getElementsByClassName("rq_choices_container")[0];
+    choices.style.display = "none";
+    let arrow = q.getElementsByClassName("arrow");
+    arrow[0].style.display = "inline-block";
+    arrow[1].style.display = "none";
 }
 
 /**
