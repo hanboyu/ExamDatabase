4505f78af95188b13ac1a80ea76ea579981e1517
Bill Han <hanb4@rpi.edu>
2020-03-02 19:48:10 -0500

    Improved Edit question HTML template

--- src/main/resources/static/js/EditQuestionUtility.js
+++ src/main/resources/static/js/EditQuestionUtility.js
@@ -1,53 +1,46 @@
-var tages_counter = 0;
 var choice_conuter = 1;
 
 
 function add_tag() {
-    var new_tag = prompt("New category:");
+    let new_tag = prompt("New category:");
     if ('content' in document.createElement('template')) {
-        var TagesList = document.getElementById("tagsList");
-        var temp = document.getElementById("tag_template").content.cloneNode(true);
+        let TagsList = document.getElementById("tagsList");
+        let temp = document.getElementById("tag_template").content.cloneNode(true);
         temp.querySelector("span").innerHTML = new_tag;
         temp.querySelector("textarea").defaultValue = new_tag;
         temp.querySelector("li").setAttribute("id", "tag_" + new_tag);
         temp.querySelector("button").setAttribute("onclick", "deleteTages('tag_" + new_tag + "')")
-        TagesList.appendChild(temp);
-        tages_counter++;
+        TagsList.appendChild(temp);
     } else {
         // Templates are not supported.
         console.log("Templates are not supported");
     }
 }
 
-function deleteTages(tag_id) {
-    target_tag = document.getElementById(tag_id);
+function DeleteTag(tag_id) {
+    let target_tag = document.getElementById(tag_id);
     target_tag.parentNode.removeChild(target_tag);
 }
 
 function add_choice() {
+    if ('content' in document.createElement('template')){
+        let choice_list = document.getElementById("choices_list");
+        let new_choice = document.getElementById("choice_template").content.cloneNode(true);
+        new_choice.querySelector("li").setAttribute("id", "choice_" + choice_conuter);
+        new_choice.querySelector("input").setAttribute("id", "choice_answer" + choice_conuter);
+        new_choice.querySelectorAll("i")[1].setAttribute("onclick", "DeleteChoice('choice_" + choice_conuter + "')") ;
+        choice_list.appendChild(new_choice);
+    }
+    else{
+        // Templates are not supported.
+        console.log("Templates are not supported");
+    }
     choice_conuter++;
-    var my_choices = document.getElementById("choices");
-    var new_choice_labl = String.fromCharCode(64 + choice_conuter);
-    console.log(document.getElementsByClassName("choices").length);//debug
-    //new choice container
-    var new_choice_div = document.createElement("div");
-    new_choice_div.setAttribute("class", "each_choice");
-    new_choice_div.setAttribute("id", "choice_" + new_choice_labl);
-    //textarea element
-    var new_choice_textarea = document.createElement("textarea");
-    new_choice_textarea.setAttribute("name", "choice");
-    new_choice_textarea.setAttribute("form", "question_form");
-    //file upload element
-    var new_choice_input = document.createElement("input");
-    new_choice_input.setAttribute("type", "file");
-    new_choice_input.setAttribute("name", "choice_image");
-    new_choice_input.setAttribute("form", "question_form");
-    //set up
-    new_choice_div.innerHTML = new_choice_labl + ": ";
-    new_choice_div.appendChild(new_choice_textarea);
-    new_choice_div.innerHTML += "Upload Image: ";
-    new_choice_div.appendChild(new_choice_input);
-    my_choices.appendChild(new_choice_div);
+}
+
+function DeleteChoice(choice_id) {
+    let target_choice = document.getElementById(choice_id);
+    target_choice.parentNode.removeChild(target_choice);
 }
 
 
