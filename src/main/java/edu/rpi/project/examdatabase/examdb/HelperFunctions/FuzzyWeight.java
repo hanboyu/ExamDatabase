a46f27348305d1ca9847a267ba76e4c0cda63c6f
alexander-fetterman <afyankeefan@gmail.com>
2020-10-14 11:49:18 -0400

    Added weight interface

---
+++ src/main/java/edu/rpi/project/examdatabase/examdb/HelperFunctions/FuzzyWeight.java
@@ -0,0 +1,10 @@
+package edu.rpi.project.examdatabase.examdb.HelperFunctions;
+
+/**
+ * FuzzyWeight is an interface that defines the necessary
+ *  functions to weight the results of a divide and conquer
+ *  fuzzy search
+ */
+public interface FuzzyWeight {
+
+}
