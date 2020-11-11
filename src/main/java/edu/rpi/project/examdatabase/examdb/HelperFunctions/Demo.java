29f53c622aba8867b9168c0f8ffc081937f60249
alexander-fetterman <afyankeefan@gmail.com>
2020-11-11 12:16:06 -0500

    Added more demo questions

--- src/main/java/edu/rpi/project/examdatabase/examdb/HelperFunctions/Demo.java
+++ src/main/java/edu/rpi/project/examdatabase/examdb/HelperFunctions/Demo.java
@@ -24,8 +24,13 @@ public class Demo {
         question_bodies.add( "Does oxygen exist naturally as a gas?" );
         question_bodies.add( "Is this chiral center R or S?" );
         question_bodies.add( "Something about carbohydrates" );
-        //question_bodies.add();
-        //question_bodies.add();
+        question_bodies.add( "Which molecule is nonpolar" );
+        question_bodies.add( "What is the hybridization of chlorine (Cl) in ClF_3"  );
+        question_bodies.add( "How many hydrogens are in ethyl butanoate?" );
+        question_bodies.add( "What is the IUPAC name for the following compound?" );
+        question_bodies.add( "How many isomers are there for trichlorobenzene" );
+        question_bodies.add( "What type of linkage is holding the base pairs of the DNA together in order to form the double helical structure" );
+        //question_bodies.add( "" );
 
         // Create an arraylist to return
         List<Question> res = new ArrayList<>();
