65d4a3752aada9f220c5408e31b85edaa7baf07e
Ran Xu <xricxy1314@gmail.com>
2020-11-18 21:00:51 -0500

    Changed search in database argument Added: 1. query user 2. query questions

--- src/main/java/edu/rpi/project/examdatabase/examdb/DataContainers/Database/dbaccess/QueryQuestionFromDatabase.java
+++ src/main/java/edu/rpi/project/examdatabase/examdb/DataContainers/Database/dbaccess/QueryQuestionFromDatabase.java
@@ -1,22 +1,73 @@
 package edu.rpi.project.examdatabase.examdb.DataContainers.Database.dbaccess;
 
 import edu.rpi.project.examdatabase.examdb.Objects.QueryObject;
+import edu.rpi.project.examdatabase.examdb.Objects.Question.Question;
+import edu.rpi.project.examdatabase.examdb.Objects.Question.QuestionFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class QueryQuestionFromDatabase implements Query {
+public class QueryQuestionFromDatabase<E> implements Query<E> {
     @Override
-    public List<QueryObject> doQuery(QueryParameters param) {
+    public List<E> doQuery(QueryParameters param) {
+        List<E> questions = new ArrayList<>();
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "Xuran123#");
-
+            Statement statement = con.createStatement();
+            String query = param.createQueryString();
+            ResultSet rs = statement.executeQuery(query);
+            QuestionFactory qf = QuestionFactory.getInstance();
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                String serialCode = rs.getString("SERIAL_CODE");
+                String tags = rs.getString("TAGS");
+                List<String> tagsList = this.split(tags);
+                String classID = rs.getString("CLASS_ID");
+                int permission = rs.getInt("PERMISSION");
+                String questionBody = rs.getString("QUESTION_BODY");
+                String choiceA = rs.getString("CHOICE_A");
+                String choiceB = rs.getString("CHOICE_B");
+                String choiceC = rs.getString("CHOICE_C");
+                String choiceD = rs.getString("CHOICE_D");
+                String choiceE = rs.getString("CHOICE_E");
+                String choiceAImg = rs.getString("CHOICE_A_IMAGE");
+                String choiceBImg = rs.getString("CHOICE_B_IMAGE");
+                String choiceCImg = rs.getString("CHOICE_C_IMAGE");
+                String choiceDImg = rs.getString("CHOICE_D_IMAGE");
+                String choiceEImg = rs.getString("CHOICE_E_IMAGE");
+                String answer = rs.getString("ANSWER");
+                List<String> questionImages = new ArrayList<>();
+                List<String> choiceImages = new ArrayList<>();
+                questionImages.add(choiceAImg);
+                questionImages.add(choiceBImg);
+                questionImages.add(choiceCImg);
+                questionImages.add(choiceDImg);
+                questionImages.add(choiceEImg);
+                List<String> choices = new ArrayList<>();
+                choices.add(choiceA);
+                choices.add(choiceB);
+                choices.add(choiceC);
+                choices.add(choiceD);
+                choices.add(choiceE);
+                Question q = qf.makeQuestion(serialCode, tagsList, classID, permission, questionBody, questionImages,
+                   choices, choiceImages, answer);
+                questions.add((E)q);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
+        return questions;
+    }
+
+    private List<String> split(String str) {
+        return new ArrayList<>(Arrays.asList(str.split(",", -1)));
     }
 }
--- src/main/java/edu/rpi/project/examdatabase/examdb/init.java
+++ src/main/java/edu/rpi/project/examdatabase/examdb/init.java
@@ -31,8 +31,11 @@ public class init {
         try {
             Connection con = DriverManager.getConnection(DB_ADDRESS, DB_USER, DB_PSWD);
             Statement statement = con.createStatement();
-            statement.execute("CREATE TABLE questions (ID INT NOT NULL, SERIAL_CODE INT NOT NULL, TAGS VARCHAR(255) NOT NULL," +
-               "PERMISSION VARCHAR(10) NOT NULL, QUESTION_BODY VARCHAR(8000) NOT NULL, CHOICE_A VARCHAR )");
+            statement.execute("CREATE TABLE questions (ID INT NOT NULL, SERIAL_CODE VARCHAR(255) NOT NULL, TAGS VARCHAR(255) NOT NULL, CLASS_ID VARCHAR(255) NOT  NULL," +
+               "PERMISSION INT NOT NULL, QUESTION_BODY VARCHAR(8000) NOT NULL, CHOICE_A VARCHAR(8000), CHOICE_B VARCHAR(8000), " +
+               "CHOICE_C VARCHAR(8000), CHOICE_D VARCHAR(8000), CHOICE_E VARCHAR(8000), CHOICE_A_IMAGE VARCHAR(8000)," +
+               "CHOICE_B_IMAGE VARCHAR(8000), CHOICE_C_IMAGE VARCHAR(8000), CHOICE_D_IMAGE VARCHAR(8000), CHOICE_E_IMAGE VARCHAR(8000)," +
+               "ANSWER VARCHAR(2), PRIMARY KEY(ID))");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -40,5 +43,6 @@ public class init {
 
     public static void main(String[] args) {
         createUserTable();
+        createQuestionTable();
     }
 }
